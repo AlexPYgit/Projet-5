f31a2104bda3d864b12a215238777536e8002507
AlexPYgit <70750015+AlexPYgit@users.noreply.github.com>
2021-04-29 09:49:36 +0200

    controllers

---
+++ controllers/cardsProduits.js
@@ -0,0 +1,16 @@
+
+//  let cardTotal = 
+//  <div class="card mb-3" style="max-width: 540px;">
+//     <div class="row g-0">
+//       <div class="col-md-4">
+//         <img class="rounded mx-auto d-block" style="width: 100%;" src="images/teddy_1.jpg" alt="ours en peluche">
+//       </div>
+//       <div class="col-md-8">
+//         <div class="card-body">
+//           <h5 class="card-title">Card title</h5>
+//           <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+//         </div>
+//       </div>
+//     </div>
+//   </div>
+//   ;
